af39a407a8604f503597088f7ef20f0d55c92ff1
M. Feliz <m.felis816@gmail.com>
2026-05-24 16:57:55 +0800

    replace service account key and use env

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -4,7 +4,7 @@ import path from "path";
 
 const serviceAccountPath = path.resolve("serviceAccountKey.json");
 
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
+const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
